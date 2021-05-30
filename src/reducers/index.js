8bd9313b7f5c363fa231ab38f07ba6b8ca8fb2fe
maurohaidr <maurohaidr@gmailcom>
2021-05-30 10:45:48 -0300

    reset fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -26,8 +26,11 @@ const initialState = {
     if (action.type === "RESET") {
       return {
         ...state,
-        timer: 0,
-        hits: 0
+        hits: 0,
+        cucaPos:{
+          x: window.innerHeight/2,
+          y: window.innerWidth/2,
+        },
       }
     }
     if(action.type === "START"){
@@ -39,6 +42,40 @@ const initialState = {
         timer: setInterval(startTimer(), 1000)
         }
   }
+    if(action.type === "LINTERNA"){
+      var f = function() {
+        var follower, init, mouseX, mouseY, positionElement, printout, timer;      
+        follower = document.getElementById('follower');      
+        printout = document.getElementById('printout');      
+        mouseX = (event) => {
+          return event.clientX;
+        };
+      
+        mouseY = (event) => {
+          return event.clientY;
+        };
+      
+        positionElement = (event) => {
+          var mouse;
+          mouse = {
+            x: mouseX(event),
+            y: mouseY(event)
+          };
+          follower.style.top = mouse.y + 'px';
+          return follower.style.left = mouse.x + 'px';
+        };
+      
+        timer = false;
+      
+        window.onmousemove = init = (event) => {
+          var _event;
+          _event = event;
+          return timer = setTimeout(() => {
+            return positionElement(_event);
+          }, 1);
+        };        
+      }.call(this);
+    }
     return state;
   }
   
